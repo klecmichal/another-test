06c2e387fdafc8b1889771c05efc85faac887560
Michal Klec <michalklec@yahoo.com>
2023-02-28 13:53:09 +0100

    Changed all var to let

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var items = ["Buy Food", "Cook Food", "It Food"];
+let items = ["Buy Food", "Cook Food", "It Food"];
 
 app.get('/', (req, res) => {
 
-  var today = new Date();
+  let today = new Date();
   
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  var let = today.toLocaleDateString("en-US", options);
  
   
  
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 })
