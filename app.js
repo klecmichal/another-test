03771fb0f10306a22c07685fec1b8138b76217b0
Michal Klec <michalklec@yahoo.com>
2023-02-28 13:34:12 +0100

    Added ejs module, post and get methods and logic to app

--- app.js
+++ app.js
@@ -2,10 +2,39 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = ["Buy Food", "Cook Food", "It Food"];
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  var today = new Date();
+  
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+ 
+  
+ 
+  res.render("list", {kindOfDay: day, newListItems: items
+  
+});
+
+});
+
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
